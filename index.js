dd8d605258cf57788db29b3bedc4c5c49a8ccf9b
Rehan Rauf <rehan@venexel.com>
2022-05-16 23:47:13 -0400

    reverted changes to index

--- index.js
+++ index.js
@@ -6,133 +6,131 @@ const customerUtils = require("./utils/CustomerUtils");
 
 (async () =>{
     try{
-        // await models.UserSession.create({authToken: "nonon", userLoginId: null})
-        // console.log(await models.UserSession.findAll())
         // await models.sequelize.sync({alter: true});
-        // await models.sequelize.sync({force: true});
+        await models.sequelize.sync({force: true});
         
-        // // CREATE VENDORS
-        // let vendorTom = await vendorUtils.createVendor({name: "Tom", email: "tom@gmail.com", password: "secure123"})
-        // let vendorKatie = await vendorUtils.createVendor({name: "Katie", email: "katie@gmail.com", password: "abcd123"})
-        // let vendorDarcy = await vendorUtils.createVendor({name: "Darcy", email: "darcy@headphones.com", password: "abc123"})
+        // CREATE VENDORS
+        let vendorTom = await vendorUtils.createVendor({name: "Tom", email: "tom@gmail.com", password: "secure123"})
+        let vendorKatie = await vendorUtils.createVendor({name: "Katie", email: "katie@gmail.com", password: "abcd123"})
+        let vendorDarcy = await vendorUtils.createVendor({name: "Darcy", email: "darcy@headphones.com", password: "abc123"})
         
-        // // CREATE BOOKS
-        // let bookLBT =  await bookUtils.createBook(
-        //     {
-        //         title: "Little Blue Truck",
-        //         isbn: "245029-13729-298",
-        //         author: "BlueTruckAuthor",
-        //         price: 25,
-        //         vendorId: vendorTom.id
-        //     });
+        // CREATE BOOKS
+        let bookLBT =  await bookUtils.createBook(
+            {
+                title: "Little Blue Truck",
+                isbn: "245029-13729-298",
+                author: "BlueTruckAuthor",
+                price: 25,
+                vendorId: vendorTom.id
+            });
         
-        // let bookBBB = await bookUtils.createBook(
-        //     {
-        //         title: "Beep Beep Book",
-        //         isbn: "245029-13729-299",
-        //         author: "BlueTruckAuthor",
-        //         price: 20,
-        //         vendorId: vendorKatie.id
-        //     }
-        // );
+        let bookBBB = await bookUtils.createBook(
+            {
+                title: "Beep Beep Book",
+                isbn: "245029-13729-299",
+                author: "BlueTruckAuthor",
+                price: 20,
+                vendorId: vendorKatie.id
+            }
+        );
         
-        // let bookBN = await bookUtils.createBook(
-        //     {
-        //         title: "BottleNeck",
-        //         isbn: "245029-13739-299",
-        //         author: "KK Tom",
-        //         price: 10,
-        //         vendorId: vendorTom.id
-        //     }
-        // );
-        // let bookBF = await bookUtils.createBook(
-        //     {
-        //         title: "Best Friends",
-        //         isbn: "245029-13739-399",
-        //         author: "Beet",
-        //         price: 12,
-        //         vendorId: vendorKatie.id
-        //     }
-        // );
-        // let bookTIWH = await bookUtils.createBook(
-        //     {
-        //         title: "This is what happens",
-        //         isbn: "245029-13739-299",
-        //         author: "Beet",
-        //         price: 10,
-        //         vendorId: vendorTom.id
-        //     }
-        // );
-        // let bookST = await bookUtils.createBook(
-        //     {
-        //         title: "Smiling Teeth",
-        //         isbn: "245019-13739-299",
-        //         author: "Pete Pan",
-        //         price: 14,
-        //         vendorId: vendorDarcy.id
-        //     }
-        // );
-        // let bookSat = await bookUtils.createBook(
-        //     {
-        //         title: "Saturday",
-        //         isbn: "245129-13739-299",
-        //         author: "Teek Tan",
-        //         price: 17,
-        //         vendorId: vendorTom.id
-        //     }
-        // );
+        let bookBN = await bookUtils.createBook(
+            {
+                title: "BottleNeck",
+                isbn: "245029-13739-299",
+                author: "KK Tom",
+                price: 10,
+                vendorId: vendorTom.id
+            }
+        );
+        let bookBF = await bookUtils.createBook(
+            {
+                title: "Best Friends",
+                isbn: "245029-13739-399",
+                author: "Beet",
+                price: 12,
+                vendorId: vendorKatie.id
+            }
+        );
+        let bookTIWH = await bookUtils.createBook(
+            {
+                title: "This is what happens",
+                isbn: "245029-13739-299",
+                author: "Beet",
+                price: 10,
+                vendorId: vendorTom.id
+            }
+        );
+        let bookST = await bookUtils.createBook(
+            {
+                title: "Smiling Teeth",
+                isbn: "245019-13739-299",
+                author: "Pete Pan",
+                price: 14,
+                vendorId: vendorDarcy.id
+            }
+        );
+        let bookSat = await bookUtils.createBook(
+            {
+                title: "Saturday",
+                isbn: "245129-13739-299",
+                author: "Teek Tan",
+                price: 17,
+                vendorId: vendorTom.id
+            }
+        );
 
 
             
-        // // CREATE CUSTOMERS 
-        // let customerSuzy = await customerUtils.createCustomer(
+        // CREATE CUSTOMERS 
+        let customerSuzy = await customerUtils.createCustomer(
+            {
+                name: "Suzy Dan",
+                address: "London ON",
+                email: "suzy@hotmail.com",
+                password: "abc1234"
+            }
+        );
+        let customerPolly = await customerUtils.createCustomer(
+            {
+                name: "Polly",
+                address: "Pine Nut Avenue",
+                email: "polly@winners.com",
+                password: "abc123"
+            }
+        );
+        let customerPinky = await customerUtils.createCustomer(
+            {
+                name: "Pinky Patrick",
+                address: "Waterloo ON",
+                email: "pinky@hotmail.com",
+                password: "abc1234"
+            });
+
+
+        // CREATE ORDER - customer can create order with multiple books
+        let firstOrder = await orderUtils.createOrder(
+            {
+                customerId: customerSuzy.id, 
+                bookIds: [bookLBT.id, bookBBB.id, bookTIWH.id]
+            }
+        );
+
+        // let secondOrder = await orderUtils.createOrder(
         //     {
-        //         name: "Suzy Dan",
-        //         address: "London ON",
-        //         email: "suzy@hotmail.com",
-        //         password: "abc1234"
+        //         customerId: customerPinky.id, 
+        //         bookIds: [bookSat.id, bookBN.id]
         //     }
         // );
-        // let customerPolly = await customerUtils.createCustomer(
-        //     {
-        //         name: "Polly",
-        //         address: "Pine Nut Avenue",
-        //         email: "polly@winners.com",
-        //         password: "abc123"
-        //     }
-        // );
-        // let customerPinky = await customerUtils.createCustomer(
-        //     {
-        //         name: "Pinky Patrick",
-        //         address: "Waterloo ON",
-        //         email: "pinky@hotmail.com",
-        //         password: "abc1234"
-        //     });
 
-
-        // // CREATE ORDER - customer can create order with multiple books
-        // let firstOrder = await orderUtils.createOrder(
+        // let thirdOrder = await orderUtils.createOrder(
         //     {
-        //         customerId: customerSuzy.id, 
-        //         bookIds: [bookLBT.id, bookBBB.id, bookTIWH.id]
+        //         customerId: customerPolly.id, 
+        //         bookIds: [bookSat.id, bookBBB.id, bookST.id]
         //     }
         // );
 
-        // // let secondOrder = await orderUtils.createOrder(
-        // //     {
-        // //         customerId: customerPinky.id, 
-        // //         bookIds: [bookSat.id, bookBN.id]
-        // //     }
-        // // );
-
-        // // let thirdOrder = await orderUtils.createOrder(
-        // //     {
-        // //         customerId: customerPolly.id, 
-        // //         bookIds: [bookSat.id, bookBBB.id, bookST.id]
-        // //     }
-        // // );
-
-        // await firstOrder.printOrder()
+        await firstOrder.printOrder()
 
         // await firstOrder.destroy()
         // await secondOrder.printOrder()
